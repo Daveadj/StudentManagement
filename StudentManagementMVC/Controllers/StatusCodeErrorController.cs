427dbf89908e354cbb5f94fbf3f689065c511d86
Daveadj <ovoadjerese@gmail.com>
2024-04-12 00:20:56 +0100

    Add project files.

---
+++ StudentManagementMVC/Controllers/StatusCodeErrorController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagementMVC.Controllers
+{
+    public class StatusCodeErrorController : Controller
+    {
+        [HttpGet("/StatusCodeError/{statusCode}")]
+        public ActionResult Error(int statuscode)
+        {
+            switch (statuscode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "404 Page Not Found";
+                    break;
+
+                case 400:
+                    ViewBag.ErrorMessage = "Bad Request";
+                    break;
+            }
+            return View();
+        }
+
+        [Route("StatusCodeError")]
+        [AllowAnonymous]
+        public IActionResult ServerError()
+        {
+            var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            ;
+
+            return View();
+        }
+    }
+}
